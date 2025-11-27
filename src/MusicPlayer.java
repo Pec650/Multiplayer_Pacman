f4cff0d879b4bf6248e015d4851a62d45c8786c5
imbacktosmoking <reffnfornio@gmail.com>
2025-11-27 11:01:02 +0800

    bab likes boys

---
+++ src/MusicPlayer.java
@@ -0,0 +1,38 @@
+import javax.sound.sampled.*;
+import java.net.URL;
+
+public class MusicPlayer {
+
+    private Clip clip;
+
+    public void playLoop(URL soundURL) {
+        try {
+            if (clip != null && clip.isOpen()) {
+                clip.stop();
+                clip.close();
+            }
+
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(soundURL);
+            clip = AudioSystem.getClip();
+            clip.open(audioStream);
+
+            clip.loop(Clip.LOOP_CONTINUOUSLY); // <--- loops forever
+            clip.start();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void stop() {
+        if (clip != null) {
+            clip.stop();
+            clip.close();
+        }
+    }
+
+    public boolean isPlaying() {
+        return clip.isActive();
+    }
+
+}
